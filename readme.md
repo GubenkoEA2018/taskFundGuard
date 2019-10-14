2a2b819716224f4c8e3b4fd9575caa42fe8b320b
Elena Gubenko <admincam777@gmail.com>
2019-10-14 22:43:28 +0300

    task completed

--- readme.md
+++ readme.md
@@ -8,11 +8,34 @@ Please focus on good coding practices.
 Once done, please send back the new and improved version.
 
 What to do:
-1) Make the piece "hollow" when the mouse hovers over it (hollow means only the border should be drawn with the color of the piece)
+
+1) Make the piece "hollow" when the mouse hovers over it 
+(hollow means only the border should be drawn with the color of the piece)
+RU -  Сделайте кусок "пустотелым", когда мышь наведет на него курсор
+(пустота означает, что только граница должна быть нарисована цветом фигуры)
+
+done
+
 2) The `init` function has too much code duplication - fix it 
+RU - Функция `init` имеет слишком много дублирования кода - исправьте это
+
+done
+
 3) Add a 'Reset' button that returns the piece back to its initial location
+Добавить кнопку «Сброс», которая возвращает кусок обратно в исходное положение
+
+done
+
 4) Add a 'Randomize' button that places the piece at a random location
+Добавьте кнопку «Рандомизация», которая помещает фигуру в случайном месте
+
+done
+
 5) Make sure that the piece never gets off the screen
+Убедитесь, что кусок никогда не сходит с экрана
+
+done
+
 6) When the page loads, get today's temperature from an API and set the piece's color according to the following ranges:
     - < 10 degrees: Blue
     - 11 - 20 degrees: Green
@@ -21,10 +44,40 @@ What to do:
 
     You can use this URL to retrieve the weather data for Tel-Aviv:  
     http://api.apixu.com/v1/current.json?key=dda6e762ae4f41efb7e173552192204&q=tel%20aviv
+
+done    
+    
+    Когда страница загрузится, получите текущую температуру из API и установите цвет элемента в соответствии со следующими диапазонами:
+        - <10 градусов: синий
+        - 11 - 20 градусов: зеленый
+        - 21 - 30 градусов: желтый
+        - более 30 градусов: красный
+    
+    Вы можете использовать этот URL для получения данных о погоде для Тель-Авива:
+        http://api.apixu.com/v1/current.json?key=dda6e762ae4f41efb7e173552192204&q=tel%20aviv
+   
 7) Please describe how would you make the motion of the piece smooth instead of jumpy?  
    Implement your suggestion for extra bonus!
+   
+   Пожалуйста, опишите, как бы вы сделали движение фигуры плавным, а не нервным?
+      Реализуйте свое предложение для дополнительного бонуса!
+   
+   done 
+   
 8) How would you improve the code of this project?   
 Meaning, how would you implement the same application differently (if at all) - did you have parts of the code that you didn't like? would you add anything? Use a different architecture?
 Give some suggestions!
+
+Как бы вы улучшили код этого проекта?
+То есть, как бы вы реализовали одно и то же приложение по-разному (если вообще) - были ли у вас части кода, которые вам не нравились? ты бы что-нибудь добавил? Использовать другую архитектуру?
+Дайте несколько предложений!
+
+I would write this application for a react.js.
+The main component would draw everything needed with button "Reset".
+I would create the following components:
+1) a circle
+2) motion control
+3) RandomPosition
+4) Weather
 
 Thanks and good luck!
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,9 @@
-// piece object
 const piece = (function() {
   let el = null;
   const init = function(el) {
     this.el = el;
   };
+
   const moveDelta = function(dx, dy) {
     const pos = this.el.getBoundingClientRect();
     this.el.style.left = `${pos.left + dx}px`;
@@ -11,34 +11,83 @@ const piece = (function() {
   };
   return {
     init,
-    moveDelta
+    moveDelta,
   };
 })();
 
 function handleClick(ev) {
   piece.moveDelta(parseInt(this.dataset.dx), parseInt(this.dataset.dy));
 }
+// function init() {
+//   const $btnUp = document.getElementById("btn-up");
+//   // $btnUp.dataset.dx = 0;
+//   $btnUp.dataset.dy = -100;
+//   $btnUp.addEventListener("click", handleClick);
+//   const $btnRight = document.getElementById("btn-right");
+//   $btnRight.dataset.dx = 100;
+//   // $btnRight.dataset.dy = 0;
+//   $btnRight.addEventListener("click", handleClick);
+//   const $btnDown = document.getElementById("btn-down");
+//   // $btnDown.dataset.dx = 0;
+//   $btnDown.dataset.dy = 100;
+//   $btnDown.addEventListener("click", handleClick);
+//   const $btnLeft = document.getElementById("btn-left");
+//   $btnLeft.dataset.dx = -100;
+//   // $btnLeft.dataset.dy = 0;
+//   $btnLeft.addEventListener("click", handleClick);
+// }
 
 function init() {
-  const $btnUp = document.getElementById("btn-up");
-  $btnUp.dataset.dx = 0;
-  $btnUp.dataset.dy = -100;
-  $btnUp.addEventListener("click", handleClick);
-  const $btnRight = document.getElementById("btn-right");
-  $btnRight.dataset.dx = 100;
-  $btnRight.dataset.dy = 0;
-  $btnRight.addEventListener("click", handleClick);
-  const $btnDown = document.getElementById("btn-down");
-  $btnDown.dataset.dx = 0;
-  $btnDown.dataset.dy = 100;
-  $btnDown.addEventListener("click", handleClick);
-  const $btnLeft = document.getElementById("btn-left");
-  $btnLeft.dataset.dx = -100;
-  $btnLeft.dataset.dy = 0;
-  $btnLeft.addEventListener("click", handleClick);
+  let array = ['btn-up', 'btn-right', 'btn-down', 'btn-left'];
+  for (let i=0; i < array.length; i++) {
+    const $btn = document.getElementById(array[i]);
+    switch (i) {
+      case 0:
+        $btn.dataset.dy = -100;
+        break;
+      case 1:
+        $btn.dataset.dx = 100;
+        break;
+      case 2:
+        $btn.dataset.dy = 100;
+        break;
+      case 3:
+        $btn.dataset.dx = -100;
+        break;
+    }
+    $btn.addEventListener("click", handleClick);
+  }
+
 }
 
 window.addEventListener("DOMContentLoaded", event => {
   piece.init(document.getElementById("piece"));
   init();
+  changeColor();
 });
+
+function changeColor() {
+
+    const API_KEY = '55c6b18bfb9d0aefd9f80ddf58e38a83';
+    const city = 'Tel Aviv';
+// const city = 'Oslo';
+// const city = 'London';
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+            .then(response => response.json())
+            .then(data => {
+              console.log(data);
+              temp = data.main.temp;
+              console.log(temp);
+              if(temp <=10) {
+                document.getElementById("piece").style.backgroundColor = `blue`;
+              }else if(temp <= 20){
+                document.getElementById("piece").style.backgroundColor = `green`;
+              }else if(temp <= 30) {
+                document.getElementById("piece").style.backgroundColor = `yellow`
+                // piece.init.style.background = `yellow`;
+              }else{
+                document.getElementById("piece").style.backgroundColor = `red`;
+              }
+            });
+}
